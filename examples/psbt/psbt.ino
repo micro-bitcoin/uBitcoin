130da49fb2b2a06155fc33b9532a774d166419f4
Stepan Snigirev <snigirev.stepan@gmail.com>
2020-06-10 23:18:37 +0200

    fix psbt example

--- examples/psbt/psbt.ino
+++ examples/psbt/psbt.ino
@@ -6,32 +6,40 @@
 #include "PSBT.h"
 
 HDPrivateKey hd("flight canvas heart purse potato mixed offer tooth maple blue kitten salute almost staff physical remain coral clump midnight rotate innocent shield inch ski", "");
-PSBT tx;
+PSBT psbt;
 
 void setup() {
   Serial.begin(115200);
-  tx.parse("70736274ff01007202000000014b30bba309cd2b8a3d51030f7731c283d8ca7bd03a750fd712206bf432bfe89c0000000000fdffffff02a08601000000000017a914c43ffc04e8fd52e70988da5a81347396e73882f28712bb0d0000000000160014d924a0e0e40ca2766bacba5db53cdc35eb3532d8f0e517000001011f40420f000000000016001470e54ab67c6b6c4eeb6a019a83332a43b909b1992206037b3a3a013c18228e7070c97bb5d8d2afe2fd749ee9ef77dc4ac20c208a4795a8180464238c54000080010000800000008000000000000000000000220203d3b09e954f3e05800bf3bb4aec4734f8d7e3a233d90bfd75c234df702a132bfe180464238c540000800100008000000080010000000000000000");
+  psbt.parseBase64("cHNidP8BAHICAAAAAUswu6MJzSuKPVEDD3cxwoPYynvQOnUP1xIga/Qyv+icAAAAAAD9////AqCGAQAAAAAAF6kUxD/8BOj9UucJiNpagTRzluc4gvKHErsNAAAAAAAWABTZJKDg5Ayidmusul21PNw16zUy2PDlFwAAAQEfQEIPAAAAAAAWABRw5Uq2fGtsTutqAZqDMypDuQmxmSIGA3s6OgE8GCKOcHDJe7XY0q/i/XSe6e933ErCDCCKR5WoGARkI4xUAACAAQAAgAAAAIAAAAAAAAAAAAAAIgID07CelU8+BYAL87tK7Ec0+NfjojPZC/11wjTfcCoTK/4YBGQjjFQAAIABAACAAAAAgAEAAAAAAAAAAA==");
+  // check parsing is ok
+  if(!psbt){
+    Serial.println("Failed parsing transaction");
+    return;
+  }
   Serial.println("Transactions details:");
+  // going through all outputs
   Serial.println("Outputs:");
-  for(int i=0; i<tx.tx.outputsNumber; i++){
-    Serial.print(tx.tx.txOuts[i].address(&Testnet));
-    if(tx.txOutsMeta[i].derivationsLen > 0){ // there is derivation path
+  for(int i=0; i<psbt.tx.outputsNumber; i++){
+    // print addresses
+    Serial.print(psbt.tx.txOuts[i].address(&Testnet));
+    if(psbt.txOutsMeta[i].derivationsLen > 0){ // there is derivation path
       // considering only single key for simplicity
-      PSBTDerivation der = tx.txOutsMeta[i].derivations[0];
-      if(hd.derive(der.derivation, der.derivationLen).publicKey() == der.pubkey){
+      PSBTDerivation der = psbt.txOutsMeta[i].derivations[0];
+      HDPublicKey pub = hd.derive(der.derivation, der.derivationLen).xpub();
+      if(pub.address() == psbt.tx.txOuts[i].address()){
         Serial.print(" (change) ");
       }
     }
     Serial.print(" -> ");
-    Serial.print(tx.tx.txOuts[i].btcAmount()*1e3);
+    Serial.print(psbt.tx.txOuts[i].btcAmount()*1e3);
     Serial.println(" mBTC");
   }
   Serial.print("Fee: ");
-  Serial.print(float(tx.fee())/100); // Arduino can't print 64-bit ints
+  Serial.print(float(psbt.fee())/100); // Arduino can't print 64-bit ints
   Serial.println(" bits");
-
-  tx.sign(hd);
-  Serial.println(tx); // now you can combine and finalize PSBTs in Bitcoin Core
+  
+  psbt.sign(hd);
+  Serial.println(psbt.toBase64()); // now you can combine and finalize PSBTs in Bitcoin Core
 }
 
 void loop() {
